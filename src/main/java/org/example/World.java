5bb2163f02c08f7ac3f2aaefb1125b32c936416e
Rohan Kumar Tammineedi <RTMT1067@MGTECHSOFT.IN>
2024-06-25 23:56:25 +0530

    pushing to world

--- src/main/java/org/example/World.java
+++ src/main/java/org/example/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void hello() {
+        System.out.println("Hello");
+    }
 }
