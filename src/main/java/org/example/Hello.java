8c6cd407c8a91e1f40eae36c11c69122302036e1
Rohan Kumar Tammineedi <RTMT1067@MGTECHSOFT.IN>
2024-06-25 23:44:10 +0530

    commiting only Hello

--- src/main/java/org/example/Hello.java
+++ src/main/java/org/example/Hello.java
@@ -5,4 +5,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    void hello() {
+        System.out.println("Hello");
+    }
 }
